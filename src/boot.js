ec2a244cd60c200fe64ff13544eaffe32d1f5d67
jvelez <jvelez@idealista.com>
2022-03-09 09:56:53 +0100

    todo

---
+++ src/boot.js
@@ -0,0 +1,63 @@
+/**
+ * Escena para la precarga de los assets que se usarán en el juego.
+ * Esta escena se puede mejorar añadiendo una imagen del juego y una 
+ * barra de progreso de carga de los assets
+ * @see {@link https://gamedevacademy.org/creating-a-preloading-screen-in-phaser-3/} como ejemplo
+ * sobre cómo hacer una barra de progreso.
+ */
+export default class Boot extends Phaser.Scene {
+  /**
+   * Constructor de la escena
+   */
+  constructor() {
+    super({ key: 'boot' });
+  }
+
+  /**
+   * Carga de los assets del juego
+   */
+  preload() {
+    // Con setPath podemos establecer el prefijo que se añadirá a todos los load que aparecen a continuación
+    this.load.setPath('assets/sprites/');
+    // this.load.image('platform', 'platform.png');
+    // this.load.image('base', 'base.png');
+    // this.load.image('calavera', 'calavera.png');
+    // this.load.image('star', 'star.png');
+    // this.load.image('key', 'Key1.png');
+    // this.load.image('player', 'player.png');
+    // this.load.image('piedra', 'piedra.png');
+    // this.load.image('azul','azul.jpg');
+    // this.load.image('fondo', 'background.png');
+    // this.load.image('walls',"/tilemaps/wall.png");  
+
+    //cargamos el alien
+    // this.load.atlas('alien','alien.png', 'alien.json');
+    this.load.atlas('enemigo','enemigo.png', 'enemigo.json');
+    this.load.atlas('alien','hero.png', 'hero.json');
+    
+    //SUELOS NIVEL 1 (imagenes del suelo)
+    this.load.image('suelo',"/tilemaps/suelo.png");  
+    this.load.image('subsuelo',"/tilemaps/subsuelo.png");
+    //MAPA NIVEL 1 (nombre recusro cache, fichero json)
+    this.load.tilemapTiledJSON('level1', '/tilemaps/level1.json');
+
+    
+    this.load.image('ui-heart-empty',"/ui_heart_empty.png");
+    this.load.image('ui-heart-full',"/ui_heart_full.png");
+    
+    this.load.image('corazon',"/corazon.png");
+    this.load.image('energia',"/energia.png");    
+    this.load.image('llave',"/key.png");
+
+  }
+
+  /**
+   * Creación de la escena. En este caso, solo cambiamos a la escena que representa el
+   * nivel del juego
+   */
+  create() {
+    
+    this.scene.start('level');
+    
+  }
+}
---
+++ src/game.js
@@ -0,0 +1,46 @@
+import Boot from './boot.js';
+import End from './end.js';
+import Level from './level.js';
+import Inicio from './Inicio.js'
+import Opciones from './Opciones.js';
+import Creditos from './Creditos.js';
+import parallaxScene from './parallaxScene.js';
+import GameUI from './GameUI.js'
+
+/**
+ * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
+ * la clase Game de Phaser, encargada de crear e iniciar el juego. 
+ */
+let config = {
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 480,
+    scale: {
+        mode: Phaser.Scale.FIT,  
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+    },
+    pixelArt: true,
+    scene: [Inicio,Opciones,Boot, Level,GameUI, Creditos, End],
+    physics: {
+        default: 'matter',//cambiado el arcade
+        matter: {
+            debug: true
+        }
+    }
+};
+
+let config2 = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 520,
+    pixelArt: true,
+    scene: [Inicio, Opciones, Creditos, Boot, Level, End],
+    physics: {
+        default: 'arcade',//cambiado el arcade
+        arcade: {
+            gravity: {y:200}
+        }
+    }
+};
+
+new Phaser.Game(config);
